fab6eee9c9b48b15afda387520180bf5f28e3609
byungjoo kim <byungjoo@kfit.com>
2017-05-12 18:33:01 +0900

    Modify migrate

--- database/migrations/2017_05_02_071144_profiles.php
+++ database/migrations/2017_05_02_071144_profiles.php
@@ -19,6 +19,7 @@ class Profiles extends Migration
         $table->foreign('user_id')->references('id')->on('users');
         $table->string('level', 10)->nullable();
         $table->string('portrait', 225)->nullable();
+        $table->integer('level_star_cd')->nullable();
         $table->string('quickplay_win', 10)->nullable();
         $table->string('quickplay_play', 10)->nullable();
         $table->string('quickplay_playtime', 10)->nullable();
